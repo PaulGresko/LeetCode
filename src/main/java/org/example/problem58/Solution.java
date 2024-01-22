80a952fe31bfe55461e335aa4e742af19adbec22
PaulGresko <TORTIX777@gmail.com>
2024-01-22 23:23:55 +0400

    Solution of 58 problems

--- src/main/java/org/example/problem58/Solution.java
+++ src/main/java/org/example/problem58/Solution.java
@@ -2,7 +2,9 @@ package org.example.problem58;
 
 public class Solution {
     public int lengthOfLastWord(String s) {
+
         String[] strs = s.split(" ");
+
         return strs[strs.length-1].length();
     }
 }
